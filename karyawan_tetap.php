9ae3fd9322d2dc37cdf707ae05550a3be167e9d5
MuhammadPradityaMaulanaArkananta <muhammadpraditya26@gmail.com>
2026-06-22 13:55:24 +0700

    [Tahap 3 & 4] Implementasi Abstraksi (Abstraction) & Implementasi Pewarisan (Inheritance)

--- karyawan_tetap.php
+++ karyawan_tetap.php
@@ -2,11 +2,9 @@
 require_once 'karyawan.php';
 
 class KaryawanTetap extends Karyawan {
-    // Properti tambahan spesifik karyawan tetap
     private $tunjanganKesehatan;
     private $opsiSahamID;
 
-    // Konstruktor untuk memetakan data dari database ke properti objek
     public function __construct($data) {
         parent::__construct();
         $this->id_karyawan         = $data['id_karyawan'];
@@ -14,18 +12,14 @@ class KaryawanTetap extends Karyawan {
         $this->departemen           = $data['departemen'];
         $this->hariKerjaMasuk       = $data['hari_kerja_masuk'];
         $this->gajiDasarPerHari     = $data['gaji_dasar_per_hari'];
-        
-        // Memetakan properti spesifik
         $this->tunjanganKesehatan  = $data['tunjangan_kesehatan'];
         $this->opsiSahamID          = $data['opsi_saham_id'];
     }
 
-    // Metode Query Spesifik (Berbasis Static agar aman dari warning)
     public static function getDaftarTetap() {
         $db = new Database();
         $query = "SELECT * FROM tabel_karyawan WHERE jenis_karyawan = 'tetap'";
         $result = $db->koneksi->query($query);
-        
         $daftar = [];
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
@@ -35,16 +29,18 @@ class KaryawanTetap extends Karyawan {
         return $daftar;
     }
 
-    // Implementasi wajib dari metode abstrak induk
+    // =========================================================================
+    // METHOD OVERRIDING - KARYAWAN TETAP
+    // =========================================================================
     public function hitungGajiBersih() {
-        return $this->hariKerjaMasuk * $this->gajiDasarPerHari;
+        // Kehadiran harian + tunjangan kesehatan bervariasi
+        return ($this->hariKerjaMasuk * $this->gajiDasarPerHari) + $this->tunjanganKesehatan;
     }
 
     public function tampilkanProfilKaryawan() {
         return "ID Saham: " . $this->opsiSahamID . " | Tunjangan Kes: Rp " . number_format($this->tunjanganKesehatan, 0, ',', '.');
     }
 
-    // Fungsi Getter untuk Dashboard View
     public function getId() { return $this->id_karyawan; }
     public function getNama() { return $this->nama_karyawan; }
     public function getDepartemen() { return $this->departemen; }
